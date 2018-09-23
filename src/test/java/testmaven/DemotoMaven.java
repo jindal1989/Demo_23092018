cc24ddc3f029672fc43caea5313e6ca1dc54b000
ankush <ankush4love@gmail.com>
2018-09-23 11:49:17 +0530

    demo_23092018 learning commit

--- src/test/java/testmaven/DemotoMaven.java
+++ src/test/java/testmaven/DemotoMaven.java
@@ -1,40 +1,52 @@
 package testmaven;
 
-import junit.framework.Assert;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 public class DemotoMaven {
-	
-	//WebDriver driver;
-	
-	@Test
-	public void hellomaven()
-	{
-		/*driver = new FirefoxDriver();
-		driver.get("https://www.facebook.com/");
-	    driver.manage().window().maximize();
-	    //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	    
-	    
-	    WebElement id = driver.findElement(By.name("email"));
-	    WebElement pass = driver.findElement(By.name("pass"));
-	    id.sendKeys("ankush4love@gmail.com");
-	    pass.sendKeys("jindal@1989");
-		driver.findElement(By.id("loginbutton")).submit();
-		driver.quit();*/
-		
+
+	public static WebDriver driver;
+
+	/*@BeforeTest
+	public void setUp() {
 		System.out.println("Welcome to world of Maven");
-		
-		WebDriver driver  = new FirefoxDriver();
-		driver.get("http://learn-automation.com/");
-		Assert.assertTrue(driver.getTitle().contains("Selenium"));
-		
-		
+
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Ankush\\Desktop\\seleniumdriver\\exe\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://facebook.com/");
+		driver.manage().window().maximize();
+	}*/
+
+	@Test(priority = 0)
+	public void verifyTitle() {
+
+		/*System.out.println(driver.getTitle());
+		Assert.assertTrue(driver.getTitle().contains("Facebook"));*/
+		System.out.println("title verified");
+
+	}
+
+	@Test(priority = 1)
+	public void loginFB() {
+
+		/*WebElement id = driver.findElement(By.name("email"));
+		WebElement pass = driver.findElement(By.name("pass"));
+		id.sendKeys("ankush4love@gmail.com");
+		pass.sendKeys("jindal@1989");
+		driver.findElement(By.xpath("//*[@value='Log In']")).submit();*/
+		System.out.println("Login to FB");
+	}
+	
+	@Test(priority = 2)
+	public void verifyUser() {
+
+		//Assert.assertTrue(driver.getPageSource().contains("Ankush"));
+		System.out.println("User verified");
 	}
 
 }
